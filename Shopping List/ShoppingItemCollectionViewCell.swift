a13fb0bebe023c6094ff96e9a24a4cc6b695c5ae
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-13 22:13:34 -0400

    Configured Shopping Item Collection View Cell

--- Shopping List/ShoppingItemCollectionViewCell.swift
+++ Shopping List/ShoppingItemCollectionViewCell.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
-}
+    @IBOutlet weak var addedLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+    var item: ShoppingItem? {
+            didSet {
+                updateViews()
+            }
+        }
+
+        func updateViews() {
+            guard let item = item else { return }
+            
+          //  imageView.image = item.
+            itemNameLabel.text = item.name
+            if item.hasBeenAdded {
+                addedLabel.text = "Added"
+            } else {
+                addedLabel.text = "Not Added"
+            }
+        }
+    }
