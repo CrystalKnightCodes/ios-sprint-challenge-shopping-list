9415e5b9a1a22176f8cfce57b68a81082917377f
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-13 21:54:21 -0400

    Created ShoppingItemCollectionViewCell class

---
+++ Shopping List/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Christy Hicks on 10/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    
+}
