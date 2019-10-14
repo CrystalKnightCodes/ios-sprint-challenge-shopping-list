4ac5339c0d333938d7985545b155d358963faeab
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-10-13 20:28:21 -0400

    Created Shopping Item Model and Shopping Item Model Controller

---
+++ Shopping List/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  Shopping List
+//
+//  Created by Christy Hicks on 10/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItem {
+    var name: String = ""
+    var hasBeenAdded: Bool = false
+}
---
+++ Shopping List/ShoppingItemController.swift
@@ -0,0 +1,13 @@
+//
+//  ShoppingItemController.swift
+//  Shopping List
+//
+//  Created by Christy Hicks on 10/13/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingItemController {
+    let itemNames = ["Apple", "Grapes", "Milk", "Muffin", "Popcorn", "Soda", "Strawberries"]
+}
